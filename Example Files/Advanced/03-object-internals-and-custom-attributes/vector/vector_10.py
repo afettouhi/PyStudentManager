f44bababb6eac62c2de1535df69eab15ba9e4a7b
André Fettouhi <A.Fettouhi@gmail.com>
2019-06-05 16:07:31 +0200

    Update of vector_10.py with better solution of custom dictionary to fill __dict__.

--- Example Files/Advanced/03-object-internals-and-custom-attributes/vector/vector_10.py
+++ Example Files/Advanced/03-object-internals-and-custom-attributes/vector/vector_10.py
@@ -6,17 +6,21 @@ import math
 
 class Vector:
 
-    def mydict(self):
-        return self.__dict__
+    def mydict(self, **coords):
+        emptydict = {}
+        for key, value in coords.items():
+            emptydict += '%s: %s' % (key, value)
+        return emptydict
+        self.__dict__ == emptydict
 
     def __init__(self, **coords):
         private_coords = {'_' + k: v for k, v in coords.items()}
-        self.mydict.update(private_coords)
+        self.__dict__.update(private_coords)
 
     def __getattr__(self, name):
         private_name = '_' + name
         try:
-            return self.mydict[private_name]
+            return self.__dict__[private_name]
         except KeyError:
             raise AttributeError('{!r} object has no attribute {!r}'.format(self.__class__.__name__, name))
 
@@ -28,8 +32,8 @@ class Vector:
 
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__,
-                               ', '.join("{k}={v}".format(k=k[1:], v=self.mydict[k])
-                                         for k in sorted(self.mydict.keys())))
+                               ', '.join("{k}={v}".format(k=k[1:], v=self.__dict__[k])
+                                         for k in sorted(self.__dict__.keys())))
 
 
 class ColoredVector(Vector):
